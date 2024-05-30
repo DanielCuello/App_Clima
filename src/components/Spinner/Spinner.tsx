0483c552427c896ef6fc545747d5b7c150cf71cc
Daniel Cuello <danielbj38@gmail.com>
2024-05-30 19:17:11 -0300

    first commit

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,16 @@
+import "./Spinner.css";
+
+export default function () {
+  return (
+    <div className="spinner">
+      <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+      </div>
+    </div>
+  );
+}
